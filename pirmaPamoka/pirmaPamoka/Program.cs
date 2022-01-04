46d282d916afbcbf63658a7644ef478635633c54
julius vaigauskas <juliusvaigauskas@juliuss-MBP-2.lan>
2022-01-04 12:56:09 +0200

    Created new application

---
+++ pirmaPamoka/pirmaPamoka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pirmaPamoka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
